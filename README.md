e9e21d980469e86626cffd8599f84b4edbd6bb53
Ayush Karanjit <aayushkaranjit2@gmail.com>
2024-09-23 22:25:17 +0545

    added the main features

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-Rock Paper Scissors played on the console
-
 The site is live at : https://ayushkaranjit.github.io/rock-paper-scissor/
--- script.js
+++ script.js
@@ -15,9 +15,12 @@ function getHumanChoice() {
 
 let computerScore = 0;
 let humanScore = 0;
+let tieCount = 0;
+let invalidCount = 0;
 
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
+    tieCount++;
     return "It's a tie!";
   } else if (
     (humanChoice === "rock" && computerChoice === "scissors") ||
@@ -33,23 +36,134 @@ function playRound(humanChoice, computerChoice) {
   ) {
     computerScore++;
     return "You lose!";
-  } else {
-    return "Invalid input!";
   }
 }
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    console.log("Round " + (i + 1));
-    const computerSelection = getComputerChoice();
-    const humanSelection = getHumanChoice();
-    console.log("The computer chooses " + computerSelection);
-    console.log("You choose " + humanSelection);
+const result = document.querySelector(".result");
 
-    console.log(playRound(humanSelection, computerSelection));
+const rock = document.querySelector(".btn1");
+
+rock.addEventListener("click", () => {
+  const computerSelection = getComputerChoice();
+  const humanSelection = "rock";
+
+  rock.classList.add("rock");
+  rock.style.color = "red";
+
+  const resultMessage = document.createElement("p");
+
+  resultMessage.textContent = `You choose ${humanSelection}. The computer chooses ${computerSelection}. ${playRound(
+    humanSelection,
+    computerSelection
+  )}`;
+
+  resultMessage.style.fontSize = "20px";
+
+  result.appendChild(resultMessage);
+
+  const score = document.createElement("p");
+
+  score.textContent = `Player Score: ${humanScore}. Computer Score: ${computerScore}. Tie Count: ${tieCount}.`;
+
+  score.style.fontSize = "20px";
+
+  result.appendChild(score);
+
+  setTimeout(() => {
+    rock.style.color = "";
+  }, 200);
+
+  winner();
+});
+
+const paper = document.querySelector(".btn2");
+
+paper.addEventListener("click", () => {
+  const computerSelection = getComputerChoice();
+  const humanSelection = "paper";
+
+  paper.classList.add("paper");
+  paper.style.color = "red";
+
+  const resultMessage = document.createElement("p");
+
+  resultMessage.textContent = `You choose ${humanSelection}. The computer chooses ${computerSelection}. ${playRound(
+    humanSelection,
+    computerSelection
+  )}`;
+
+  resultMessage.style.fontSize = "20px";
+
+  result.appendChild(resultMessage);
+
+  const score = document.createElement("p");
+
+  score.textContent = `Player Score: ${humanScore}. Computer Score: ${computerScore}. Tie Count: ${tieCount}.`;
+
+  score.style.fontSize = "20px";
+
+  result.appendChild(score);
+
+  setTimeout(() => {
+    paper.style.color = "";
+  }, 200);
+
+  winner();
+});
+
+const scissors = document.querySelector(".btn3");
+
+scissors.addEventListener("click", () => {
+  const computerSelection = getComputerChoice();
+  const humanSelection = "scissors";
+
+  scissors.classList.add("scissors");
+  scissors.style.color = "red";
+
+  const resultMessage = document.createElement("p");
+
+  resultMessage.textContent = `You choose ${humanSelection}. The computer chooses ${computerSelection}. ${playRound(
+    humanSelection,
+    computerSelection
+  )}`;
+
+  resultMessage.style.fontSize = "20px";
+
+  result.appendChild(resultMessage);
+
+  const score = document.createElement("p");
+
+  score.textContent = `Player Score: ${humanScore}. Computer Score: ${computerScore}. Tie Count: ${tieCount}.`;
+
+  score.style.fontSize = "20px";
+
+  result.appendChild(score);
+
+  setTimeout(() => {
+    scissors.style.color = "";
+  }, 200);
+
+  winner();
+});
+
+function winner() {
+  if (humanScore === 5) {
+    const confirmation = confirm(
+      "You won! Your score was 5! Do you want to play again?"
+    );
+    if (confirmation) {
+      location.reload();
+    } else {
+      alert("Thank you for playing!");
+    }
+  } else if (computerScore === 5) {
+    const confirmation = confirm(
+      "You lost! The computer's score was 5! Do you want to play again?"
+    );
+    if (confirmation) {
+      location.reload();
+    } else {
+      alert("Thank you for playing!");
+    }
   }
-  console.log("Computer score: " + computerScore);
-  console.log("Human score: " + humanScore);
 }
-
-playGame();
